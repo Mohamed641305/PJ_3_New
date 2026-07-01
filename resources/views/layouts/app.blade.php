51981c31a6876e15d1d87e40bf9847d8f9bde172
Mohamed641305 <emo332996@gmail.com>
2026-07-01 03:18:33 +0300

    وصف التحديث

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    ```
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'EduPanel') }}</title>
@@ -127,9 +126,7 @@
             }
         }
     </style>
-    ```
 
-    ```
     <div id="app">
 
         {{-- NAVBAR --}}
@@ -324,7 +321,7 @@
             });
         });
     </script>
-    ```
+
     </body>
 
 </html>
